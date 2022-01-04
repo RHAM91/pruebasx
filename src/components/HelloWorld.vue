fc7b325639174c2889dfc3a52cf82e18e5550f66
Romario Torres <romariotorres@vivaldi.net>
2022-01-03 19:59:09 -0600

    mods de juego

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>
-      Hola mundo!!!
+      Hola mundo!!! xD
     </h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
