7ec782580258bd1f6bf417a000b2d9006b10bb14
Romario Torres <romariotorres@vivaldi.net>
2022-01-03 20:04:17 -0600

    nuevas mods

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>
-      Hola mundo!!! xD
+      Hola mundo!!! xDxDxDxDxDxD
     </h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
